f239aa93fb5cc1cb7d7fa9fe498f696d2fa3dc30
Shiran <127624730+Shiranakther@users.noreply.github.com>
2025-04-10 02:07:07 +0530

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ devzora/src/main/java/com/devzora/devzora/DevzoraApplication.java
@@ -0,0 +1,13 @@
+package com.devzora.devzora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevzoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevzoraApplication.class, args);
+	}
+
+}
