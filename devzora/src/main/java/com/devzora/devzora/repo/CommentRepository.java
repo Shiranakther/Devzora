b98a8473ebb03303986c033beadf7c05ba20fcd1
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2025-05-07 12:25:02 +0530

    comment full working function

---
+++ devzora/src/main/java/com/devzora/devzora/repo/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.devzora.devzora.repo;
+
+import com.devzora.devzora.model.Comment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, String> {
+    List<Comment> findByPostId(String postId);
+}
