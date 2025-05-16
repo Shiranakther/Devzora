59c91cca8c3951fee965b6f8a1081febc3436021
Shiran <127624730+Shiranakther@users.noreply.github.com>
2025-05-16 19:50:07 +0530

    feat: Add navigation button to redirect to Ishan's page in ManageCourse

--- devzora-frontend/src/pages/ManageCourse.jsx
+++ devzora-frontend/src/pages/ManageCourse.jsx
@@ -136,6 +136,8 @@ const ManageCourse = () => {
               <div className="course-actions">
                 <Link to={`/edit-course/${course.id}`} className="edit-link" style={{width:'80px',backgroundColor:'#0080ff'}}>Edit</Link>
                 <button onClick={() => handleDelete(course.id)} className="delete-button" style={{width:'80px'}}>Delete</button>
+
+                //navigate ishans page
                 <button  className="delete-button" style={{width:'80px',backgroundColor:'green'}}
                 onClick={
                     () => {
